ac98768f2a2636e8a6198c668e603eef7b39aa3e
iLoreto <i.loreto@hotmail.com>
2021-03-30 09:24:57 -0700

    cleaned init sh for version 8.x

--- init_install.sh
+++ init_install.sh
@@ -2,15 +2,11 @@
 yum -y update 
 yum -y upgrade 
 
-yum -y install wget firewalld
-
-wget http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
-rpm -ivh epel-release-7-11.noarch.rpm
+yum -y install epel-release
+yum -y update
+yum -y upgrade
 
 yum -y install wget firewalld htop nano git tmux
-
-yum -y update 
-yum -y upgrade 
 
 systemctl enable firewalld.service
 systemctl start firewalld.service
@@ -19,11 +15,5 @@ firewall-cmd --set-default-zone=public
 firewall-cmd --add-service=ssh --zone=public --permanent
 firewall-cmd --reload
 
-rm -f *.rpm
-
-
-
-
-
-
-
+yum clean all
+exit;
