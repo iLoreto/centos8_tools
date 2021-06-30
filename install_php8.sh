3c45491a4e290bdbc10e5fb59bdbcd60d7ee106b
Ivan Loreto <i.loreto@hotmail.com>
2021-06-30 15:22:27 -0700

    patch php8 dnf use only

--- install_php8.sh
+++ install_php8.sh
@@ -1,6 +1,5 @@
-yum -y install yum-utils
-dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+yum -y install yum-utils epel-release
 dnf install -y https://rpms.remirepo.net/enterprise/remi-release-8.rpm
-yum-config-manager -y --enable remi-php80
-yum -y update
+dnf module reset php -y
+dnf module install php:remi-8.0 -y
 php -v
