e6d1678424338b22bd1ec4eee60c171e4766624a
Ivan Loreto <i.loreto@hotmail.com>
2021-06-30 15:31:23 -0700

    added opcache to php install

--- install_php8.sh
+++ install_php8.sh
@@ -1,5 +1,7 @@
-yum -y install yum-utils epel-release
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf install -y https://rpms.remirepo.net/enterprise/remi-release-8.rpm
+yum -y install yum-utils
 dnf module reset php -y
 dnf module install php:remi-8.0 -y
-php -v
+dnf -y install php80-php-opcache
+php --version
